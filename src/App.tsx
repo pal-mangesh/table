99e7f8afdb64737b890fde97230ef89cccb34fba
pal <jio4tech@gmail.com>
2021-01-18 18:54:48 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,27 @@ import Button from "./components/button";
 import Input from "./components/input";
 
 interface IListItem {
-  value?: string;
+  firstName?: string;
+  lastName?: string;
+  age?: number;
 }
 
 const App = () => {
   const [editableItem, setEditableItem] = React.useState(-1);
-  const [todoList, setTodoList] = React.useState([
-    { value: "TEMP" },
-  ] as IListItem[]);
+  const [todoList, setTodoList] = React.useState([] as IListItem[]);
 
   const addListItem = () => {
     const list = [...todoList];
-    list.push({ value: "" });
+    list.push({firstName:"", lastName:"", age:0});
     setTodoList([...list]);
   };
 
-  const onFieldValueChange = (id: number) => (value: string) => {
-    const list = [...todoList] as IListItem[];
+  const onFieldValueChange = (id: number,fieldKey:string) => (value: string) => {
+    const list = [...todoList] as any;
     let i = 0;
     for (let item of list) {
       if (i === id) {
-        item.value = value;
+        item[fieldKey] = value;
         break;
       }
       i += 1;
@@ -49,69 +49,119 @@ const App = () => {
   return (
     <div>
       <div>
-        <h3>TODO LIST APP</h3>
-      </div>
-      <div>
-        {todoList && todoList.length ? (
-          <>
-            <ul>
-              {todoList.map((li: IListItem, index: number) => (
-                <li
-                  style={{ display: "flex", alignItems: "center" }}
-                  key={index}
-                  onClick={makeEditable(index)}
-                >
-                  <div style={{ marginRight: "20px" }}>{index+1}. </div>
-                  <div>
-                    {editableItem === index ? (
-                      <>
-                        <div
-                          style={{
-                            display: "flex",
-                            alignItems: "center",
-                          }}
-                        >
-                          <div>
-                            <Input
-                              value={li.value}
-                              onChange={onFieldValueChange(index)}
-                            />
-                          </div>
-                        </div>
-                      </>
-                    ) : (
-                      li.value
-                    )}
+        <table width="100%" >
+          <tr >
+            <th>Sr no.</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Age</th>
+            <th>Edit</th>
+            <th>update</th>
+            <th>Delete</th>
+
+          </tr>
+          {todoList.map((li: IListItem, index: number) => (
+            <tr >
+              <td>
+                <div style={{ marginRight: "20px" }}>
+                  {index + 1}.
+</div>
+              </td>
+              <td>{editableItem === index ? (
+                <>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div style={{
+                      display: "flex"
+                    }}>
+                      <Input
+                        value={li.firstName}
+                        onChange={onFieldValueChange(index,"firstName")}
+                      />
+                    </div>
                   </div>
-                  <div>
-                    <Button onClick={removeItem(index)}>x</Button>
+                </>
+              ) : (
+                  li.firstName
+                )} </td>
+              <td>{editableItem === index ? (
+                <>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div style={{
+                      display: "flex"
+                    }}>
+                      <Input
+                        value={li.lastName}
+                        onChange={onFieldValueChange(index,"lastName")}
+                      />
+                    </div>
                   </div>
-                </li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          ""
-        )}
-      </div>
-      <div style={{ display: "flex", alignItems: "center" }}>
+                </>
+              ) : (
+                  li.lastName
+                )} </td>
+              <td>{editableItem === index ? (
+                <>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div style={{
+                      display: "flex"
+                    }}>
+                      <Input
+                        value={li.age+""}
+                        onChange={onFieldValueChange(index,"age")}
+                      />
+                    </div>
+                  </div>
+                </>
+              ) : (
+                  li.age
+                )} </td>
+              <td>
+                <div>
+                  <Button type="primary" onClick={makeEditable(index)}>Edit</Button>
+                </div>
+              </td>
+              <td>
+                <div>
+                  <Button type="primary" onClick={resetEditable}>update</Button>
+                </div>
+              </td>
+              <td>
+                <div>
+                  <Button type="primary" onClick={removeItem(index)}>Delete</Button>
+                </div>
+              </td>
+
+            </tr>
+          ))}
+        </table>
+        <hr />
         <div style={{ marginRight: "20px" }}>
           <Button type="secondary" onClick={addListItem}>
-            Add
-          </Button>
+            Add a new record
+</Button>
         </div>
-        <div>
-          {editableItem > 0 ? (
-            <Button type="primary" onClick={resetEditable}>
-              OK
-            </Button>
-          ) : (
-            ""
-          )}
-        </div>
+
       </div>
+
     </div>
   );
 };
 
 export default App;
+
+
